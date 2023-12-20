05623dff1491a07e6db18e3cb2cce58a15df5b91
Martin Pitt <martin.r.pitt@gmail.com>
2023-12-20 01:54:15 +0000

    Try pull in JP translation

--- scripts/lib/locales.mjs
+++ scripts/lib/locales.mjs
@@ -3,7 +3,7 @@ import { format, join } from 'node:path';
 import { GameDirectory } from './config.mjs';
 import { Glyphs } from './constants.mjs';
 import { TextDecoder } from 'node:util';
-import { getLocaleTag } from './lcid.mjs';
+import { getLocaleIdentifier, getLocaleTag } from './lcid.mjs';
 
 const VersionMinimum = 110;
 
@@ -186,6 +186,103 @@ for(let iter = 0; iter < Languages.length; ++iter)
 }
 
 
+// Gets the translation from the JP community, see: https://docs.google.com/spreadsheets/d/1U9Y3iV7pfYGvlsl_tjvxX5mN0L_YrLlxdnCNnpMAyso/
+let jp = {
+	lcid: getLocaleIdentifier('ja-JP'),
+	name: '日本語',
+	locale: 'ja-JP',
+	fallback: 1033,
+	glyph: 'CJK',
+};
+Languages.push(jp);
+let request = await fetch('https://script.google.com/macros/s/AKfycbyx-b28QMIPtIXQlazwhaWZUBtBBqSyy5M5h_h3z6oAB_3DuoZUwCjkVdjDOZTk2LIA/exec');
+let result = await request.json();
+
+Locale[jp.lcid] = result.reduce((mapping, [key, value]) => {
+	mapping[key] = value;
+	return mapping;
+}, {});
+
+
+
+
+
+
+// const descFieldNames = new Map([
+// 	[0, '采集自'],
+// 	[1, '制造于'],
+// 	[2, '燃料类型'],
+// 	[3, '能量'],
+// 	[4, '发电类型'],
+// 	[5, '发电功率'],
+// 	[6, '热效率'],
+// 	[7, '流体消耗'],
+// 	[8, '输入功率'],
+// 	[9, '输出功率'],
+// 	[10, '蓄电量'],
+// 	[11, '工作功率'],
+// 	[12, '待机功率'],
+// 	[13, '连接长度'],
+// 	[14, '覆盖范围'],
+// 	[15, '运载速度'],
+// 	[16, '接口数量'],
+// 	[17, '仓储空间'],
+// 	[18, '开采对象'],
+// 	[19, '开采速度'],
+// 	[20, '运送速度'],
+// 	[21, '货物堆叠'],
+// 	[22, '制造速度'],
+// 	[23, '血量'],
+// 	[24, '仓储物品'],
+// 	[25, '船运载量'],
+// 	[26, '船运载量'],
+// 	[27, '飞行速度'],
+// 	[28, '制造加速'],
+// 	[29, '喷涂次数'],
+// 	[30, '流体容量'],
+// 	[31, '机甲功率提升'],
+// 	[32, '采集速度'],
+// 	[33, '研究速度'],
+// 	[34, '弹射速度'],
+// 	[35, '发射速度'],
+// 	[36, '使用寿命'],
+// 	[37, '潜在能量'],
+// 	[38, '最大充能功率'],
+// 	[39, '基础发电功率'],
+// 	[40, '增产剂效果'],
+// 	[41, '喷涂增产效果'],
+// 	[42, '喷涂加速效果'],
+// 	[43, '额外电力消耗'],
+// 	[44, '配送范围'],
+// 	[45, '船运载量'],
+// 	[46, '飞行速度'],
+// 	[47, '弹药数量'],
+// 	[48, '伤害'],
+// 	[49, '射击速度'],
+// 	[50, '每秒伤害'],
+// 	[51, '防御塔类型'],
+// 	[52, '弹药类型'],
+// 	[53, '最大耐久度'],
+// 	[54, '手动制造速度'],
+// 	[55, '伤害类型'],
+// 	[56, '目标类型'],
+// 	[57, '对地防御范围'],
+// 	[58, '对天防御范围'],
+// 	[59, '飞行速度'],
+// 	[60, '射击距离'],
+// 	[61, '爆炸半径'],
+// 	[62, '主炮伤害'],
+// 	[63, '主炮射速'],
+// 	[64, '主炮射程'],
+// 	[65, '舰炮伤害'],
+// 	[66, '舰炮射速'],
+// 	[67, '舰炮射程'],
+// ])
+
+
+// function descField(field, item) {
+// 	return 
+// }
 
 
 // let currentLanguage = null;
@@ -194,6 +291,9 @@ for(let iter = 0; iter < Languages.length; ++iter)
 // export function getLanguages() { return Languages }
 // export function Translate(key) { return Strings.get(currentLanguage.lcid)[key] || key }
 // export function TranslateParam(key, fallback) { return Strings.get(currentLanguage.lcid)[key] || fallback }
+
+
+
 
 
 
